243a686f7855e9c996911151753e639292ef172b
Shayant Sital <shayant@gmail.com>
2020-02-02 01:18:39 -0300

    localstorage

--- main.js
+++ main.js
@@ -10,8 +10,41 @@ const setSelected = target => {
   target.classList.toggle("selected");
 };
 
+const saveSeatsToLocalStorage = selectedSeats => {
+  const seatIndex = selectedSeats.map(seat => [...seats].indexOf(seat));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatIndex));
+};
+
+const saveMovieToLocalStorage = movieIndex => {
+  localStorage.setItem("movieIndex", movieIndex);
+};
+const savePriceToLocalStorage = movieIndex => {
+  localStorage.setItem("price", movieIndex);
+};
+
+const getLocalStorageData = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  const selectedMovie = localStorage.getItem("movieIndex");
+  const moviePrice = localStorage.getItem("price");
+
+  if (selectedSeats.length !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+  if (selectedMovie !== null) {
+    moviesSelect.selectedIndex = selectedMovie;
+    ticketPrice = moviePrice;
+  }
+  updateSeatCount();
+  updateTotal();
+};
+
 const updateSeatCount = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  saveSeatsToLocalStorage([...selectedSeats]);
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
 };
@@ -20,9 +53,16 @@ const updateTotal = () => {
   total.innerText = count.innerText * ticketPrice;
 };
 
+getLocalStorageData();
+
+saveMovieToLocalStorage(moviesSelect.selectedIndex);
+savePriceToLocalStorage(moviesSelect.value);
+
 moviesSelect.addEventListener("change", e => {
   ticketPrice = e.target.value;
   updateTotal();
+  saveMovieToLocalStorage(e.target.selectedIndex);
+  savePriceToLocalStorage(e.target.value);
 });
 
 container.addEventListener("click", e => {
